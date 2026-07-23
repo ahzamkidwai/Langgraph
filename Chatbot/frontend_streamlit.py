bead9b84130977361ad9419c17e42d6c5793c1d0
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-07-23 11:05:26 +0530

    [Chatbot]: Add streaming for large response in LLM

--- Chatbot/frontend_streamlit.py
+++ Chatbot/frontend_streamlit.py
@@ -23,8 +23,19 @@ if user_input:
         st.text(user_input)
         
         
-    response = workflow.invoke({ 'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-    ai_message = response['messages'][-1].content
-    message_history.append({'role': 'assistant', 'content': ai_message})
+    # response = workflow.invoke({ 'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    # ai_message = response['messages'][-1].content
+    # message_history.append({'role': 'assistant', 'content': ai_message})
+    # with st.chat_message('assistant'):
+    #         st.text(ai_message)
+    
     with st.chat_message('assistant'):
-            st.text(ai_message)
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in workflow.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config={'configurable': {'thread_id': 'thread-1'}},
+                stream_mode='messages'
+            )
+        )
+    
+    message_history.append({'role': 'assistant', 'content': ai_message})
